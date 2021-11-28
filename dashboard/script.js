0595ede52ce996034cbdd13bdae5c995ac912f41
Seth <sethlee@utexas.edu>
2021-11-28 15:25:54 -0600

    dashboard more

--- dashboard/script.js
+++ dashboard/script.js
@@ -15,6 +15,12 @@ function populateSwarmList(swarm_list, dict) {
 
         item.appendChild(text)
 
+        var spinner = document.createElement('span');
+        spinner.className = 'spinner-grow text-secondary spinner-grow-sm ml-2';
+        spinner.style.display = 'none';
+        spinner.id = 'spinner-' + swarm_names[i];
+        item.appendChild(spinner);
+
         // Add it to the list:
         swarm_list.appendChild(item);
     }
@@ -55,6 +61,41 @@ function getSwarmInfo(name) {
     return deff
 }
 
+function getSwarmMetric(name) {
+    deff = $.ajax({
+        type: "GET", 
+        url: "http://3.83.149.203:5000/swarm-metric",
+        async: true, 
+        data: {
+            'swarm-name': name
+        },
+        datatype: "json",
+        success: function(result, status, xhr){
+            return result;
+        },
+        error: function(xhr, status, err) {
+          console.log(status + ": " + err);
+        }
+      });
+    return deff
+}
+
+function getRunningSwarms() {
+    deff = $.ajax({
+        type: "GET", 
+        url: "http://3.83.149.203:5000/check-running-swarm",
+        async: true, 
+        datatype: "json",
+        success: function(result, status, xhr){
+            return result;
+        },
+        error: function(xhr, status, err) {
+          console.log(status + ": " + err);
+        }
+      });
+    return deff
+}
+
 function appendSwarmToTable(table, swarm_data) {
     while (table.firstChild) {
         table.removeChild(table.firstChild);
@@ -70,12 +111,12 @@ function appendDeviceToTable(table, data) {
     var row_html = '<th scope="row">';
     row_html += data['DeviceId']['N'];
     row_html += '</th>';
-//['DeviceProgress', 'N'],
     var key_type = [['DeviceStatus', 'S'], ['EncIdx', 'N'], ['Hostname', 'S']];
     for (var i = 0; i < key_type.length; i++) {
         row_html += '<td>';
         if (key_type[i][0] == 'Hostname') {
-            row_html += data[key_type[i][0]][key_type[i][1]].split('-')[3];
+            row_html += data[key_type[i][0]][key_type[i][1]].split('-')[2];
+            row_html += '-' + data[key_type[i][0]][key_type[i][1]].split('-')[3];
         }
         else {
             row_html += data[key_type[i][0]][key_type[i][1]];
@@ -95,7 +136,34 @@ function appendDeviceToTable(table, data) {
         row.style.backgroundColor = '#1A1F29';
         row.style.color = '#CCCAC2'
     }
+    row.onclick = function() {
+        console.log(data['DeviceId']['N'] + ' clicked')
+    };
+    row.appendChild(getProgressbarSpace(data['DeviceStatus']['S'], data['EncIdx']['N'], data['TotalEncIdx']['N']))
     table.appendChild(row)
+}
+
+function getProgressbarSpace(status, encIdx, totalEncIdx) {
+    var percentage = encIdx / totalEncIdx * 100;
+    if (status == 'Finished') {
+        percentage = 100;
+    }
+    console.log(percentage)
+    var progressbar_space = document.createElement('td');
+    progressbar_space.setAttribute('style', 'vertical-align: middle;')
+    var progress = document.createElement('span');
+    progress.className = 'progress';
+    progress.setAttribute('style', 'height: 5px; width: 85%;')
+    var progressbar = document.createElement('span');
+    progressbar.className = 'progress-bar progress-bar-striped bg-success';
+    if (percentage < 100) {
+        progressbar.className += ' progress-bar-animated';
+    }
+    progressbar.setAttribute('style', 'width:'+ percentage + '%;')
+    progressbar.setAttribute('role', 'progressbar')
+    progress.appendChild(progressbar)
+    progressbar_space.appendChild(progress)
+    return progressbar_space
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -120,38 +188,95 @@ $(document).ready(function () {
                         active_nav_item = $(".nav").find(".active")
                         if (active_nav_item.length > 0) {
                             selected_swarm_name = active_nav_item[0].id.split('-')[2]
-                            console.log(selected_swarm_name) //@TODO call when rendering swarm page
-                            document.getElementById('swarm-info-text').innerHTML = 'Simulated Devices Status';
+                            document.getElementById('swarm-chart-column').removeAttribute('hidden');
+                            document.getElementById('swarm-info-text').innerHTML = 'Simulated Devices Status ' 
+                            document.getElementById('swarm-loading').style.display = '';
+                            document.getElementById('graph-loading').style.display = '';
+
+                            // var swarm_details = document.getElementById('swarmdet');
+                            // while (swarm_details.firstChild) {
+                            //     swarm_details.removeChild(swarm_details.firstChild);
+                            // }
+
+                            fetch('example.json')
+                                .then((res)=> {
+                                return res.text();
+                                })
+                                .then((data) => {
+                                const tree = JsonView.createTree(data);
+                                JsonView.render(tree, document.querySelector('.swarmdet'));
+                                JsonView.collapseChildren(tree);
+                                })
+                                .catch((err) => {
+                                console.log(err);
+                            });
                         }
                     }
                 )
             });
         }
     )
+    
+    // initialize chart
+    const ctx = document.getElementById('swarm-chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+                datasets: [{
+                    label: "swarm",
+                    data: [],
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 1,
+                    showLine: true,
+                    pointRadius: 1,
+                    hoverRadius: 1
+                }],
+            },
+        options: {
+            responsive: true
+        }
+    });
+
 
     $(function(){
         $("#swarm").load("swarm.html", null, function() {
             var table = document.getElementById("devices");
                 setInterval(function() {
                     if (selected_swarm_name != null) {
+                        document.getElementById('devices-table').removeAttribute('hidden');
+                        document.getElementById('welcome').style.display = 'none';
+                        document.getElementById('swarm-loading').style.display = 'none';
+                        // update swarm info
                         getSwarmInfo(selected_swarm_name).done(function(data) {
                             appendSwarmToTable(table, data['devices']);
                         });
                     }
+                    // check which swarm is currently running
+                    getRunningSwarms().done(function(data) {
+                        for (var i=0; i < data['runningSwarms'].length; i++) {
+                            document.getElementById('spinner-'+data['runningSwarms'][i]).style.display = '';
+                        }
+                    });
 
                 }, 2000);
+                setInterval(function() {
+                    if (selected_swarm_name != null) {
+                        // update chart
+                        getSwarmMetric(selected_swarm_name).done(function(data) {
+                            myData = [];
+                            for (var i=0; i < data['times'].length; i++) {
+                                myData.push({x: data['times'][i], y: data['accs_list'][i]});
+                            }
+                            myChart.data.datasets[0].label = selected_swarm_name;
+                            myChart.data.datasets[0].data = myData;
+                            myChart.update('active');
+                            document.getElementById('graph-loading').style.display = 'none';
+                        });
+
+                        
+                    }
+                }, 4000);
         });
     });
-
-    var data = [{
-        id: 1,
-        status: 'Item 1',
-        progress: 'good'
-    }, {
-        id: 2,
-        status: 'Item 2',
-        progress: 'bad'
-    }]; 
-
-    
 });
+
