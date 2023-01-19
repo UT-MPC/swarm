f2d6f8295f96719c776043ac4899a9719c844e34
Seth Lee <sethlee@utexas.edu>
2023-01-19 12:45:29 -0600

    fix: resnet support

--- device/exp_device.py
+++ device/exp_device.py
@@ -20,15 +20,19 @@ class GreedyValidationClient(device.base_device.Device):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def delegate(self, other, epoch, iteration):
+    def delegate(self, other, epoch, iteration, fit_to_batch=False):
         new_weights = self.fit_weights_to(self._weights, other, 1)
         new_weights = self.fit_weights_to(new_weights, self, 1)
         hist = self.eval_weights(new_weights)
         if self._last_hist[0] > hist[0]:
             self._weights = new_weights
             for _ in range(iteration-1):
-                self._weights = self.fit_to(other, 1)
-                self._weights = self.fit_to(self, 1)
+                if fit_to_batch:
+                    self._weights = self.fit_to_batch(other, 1)
+                    self._weights = self.fit_to_batch(self, 1)
+                else:
+                    self._weights = self.fit_to(other, 1)
+                    self._weights = self.fit_to(self, 1)
 
 class GreedyWOSimDevice(device.base_device.Device):
     """
